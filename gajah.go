49be3e27156cf2b7eea1a223c368e315344313fb
mzaini30 <muhzaini30@gmail.com>
2022-02-01 17:04:21 +0800

    Upload

--- gajah.go
+++ gajah.go
@@ -3,9 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"log"
 )
 
 func main(){
+	// Ambil port
 	port := os.Args[1]
 	fmt.Println(port)
+
+	// Ambil file
+	err := filepath.Walk(".",
+	    func(path string, info os.FileInfo, err error) error {
+	    if err != nil {
+	        return err
+	    }
+	    fmt.Println(path, info.Size())
+	    return nil
+	})
+	if err != nil {
+	    log.Println(err)
+	}
 }
