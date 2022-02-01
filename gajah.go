2984f0f7613cc17e04476715bee2da4e2524e944
mzaini30 <muhzaini30@gmail.com>
2022-02-01 18:24:21 +0800

    Upload

--- gajah.go
+++ gajah.go
@@ -6,13 +6,15 @@ import (
 	"path/filepath"
 	// "log"
 	"strings"
+	"net/http"
+	"io/ioutil"
 )
 
 func main(){
 	// Ambil port
-	// port := os.Args[1]
+	port := os.Args[1]
 
-	// Ambil file
+	// Ambil file: file_php
 	var semua_file = [] string {}
 	filepath.Walk(".", func(x string, _ os.FileInfo, _ error) error {
 		semua_file = append(semua_file, x)
@@ -23,5 +25,11 @@ func main(){
 		if strings.Contains(semua_file[n], ".php"){
 			file_php = append(file_php, semua_file[n])
 		}
+	}
+
+	for n := range file_php {
+		data, _ := http.Get("http://localhost:", port, "/", file_php[n])
+		nama_file := strings.Replace(file_php[n], ".php", ".html", -1)
+		ioutil.WriteFile(nama_file, [] byte (data), 0755)
 	}
 }
